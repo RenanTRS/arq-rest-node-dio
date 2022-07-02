9a63738d5cdf537652611def4a8d54d4d2caf539
Renan Souza <renantrsouza@gmail.com>
2022-07-02 16:51:28 -0400

    ADD import routes

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,11 @@
 import express from 'express'
+import { statusRoute } from './routes/status.route'
+import { usersRoute } from './routes/users.route'
+
 const app = express()
+
+app.use(express.json()) //Uso do json
+app.use(statusRoute) //Rota do status
+app.use(usersRoute) //Rotas de usuário
+
 app.listen(3333, () => console.log('Server is running on port 3333'))
