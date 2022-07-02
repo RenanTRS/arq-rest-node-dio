c9be8967f10900e80c89f6bf92e9d283e59c3735
Renan Souza <renantrsouza@gmail.com>
2022-07-02 15:09:46 -0400

    CREATE users id route

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -2,7 +2,22 @@ import { NextFunction, Request, Response, Router } from 'express'
 
 export const usersRoute = Router()
 
+//GET
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
   const users = [{ userName: 'Renan' }]
   res.status(200).json(users)
+})
+
+usersRoute.get(
+  '/users/:uuid',
+  (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+    const id = req.params.uuid
+    res.status(200).send({ id })
+  }
+)
+
+//POST
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+  const newUser = req.body
+  res.status(201).send(newUser)
 })
